ba28f92aaff08230dd4e33867f6546c4e1500f68
Sondre - EV-H <sondre.erno@evenoit.no>
2023-05-10 23:30:52 +0200

    flatpickr changed default options. Added missing html view in cropper

--- aurelia1-plugins/image-cropper/elements/cropper-element.ts
+++ aurelia1-plugins/image-cropper/elements/cropper-element.ts
@@ -3,98 +3,91 @@ import Cropper from 'cropperjs';
 
 @customElement('cropper')
 export class CropperCustomElement {
-  public element: HTMLImageElement;
-  public cropper: Cropper;
+    public element: HTMLImageElement;
+    public cropper: Cropper;
 
-  @bindable public dragMode: Cropper.DragMode = 'move';
-  dragModeChanged(dragMode: Cropper.DragMode) {
-    if (this.cropper?.setDragMode) {
-      this.cropper.setDragMode(dragMode);
-    }
-  }
-
-  @bindable({ defaultBindingMode: bindingMode.oneTime }) public responsive: boolean = true;
-  @bindable({ defaultBindingMode: bindingMode.oneTime }) public maxHeight: Number = 85;
-
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public options: Cropper.Options<any>;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public src: string;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public title: string;
-
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public cropStart: (event: CustomEvent) => void;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public cropMove: (event: CustomEvent) => void;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public cropEnd: (event: CustomEvent) => void;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public crop: (event: CustomEvent) => void;
-  @bindable({ defaultBindingMode: bindingMode.oneWay }) public zoom: (event: CustomEvent) => void;
-
-  private attached() {
-    //if (this.element) this.element.addEventListener('ready', () => { console.log("cropperReady"); });
-    //if (!this.maxHeight) this.maxHeight = this.element.parentElement.top
-  }
-
-  private detached() {
-    this.cropper.destroy();
-  }
-
-  getData() {
-    console.log("CropBoxData", this.cropper.getCropBoxData());
-    console.log("getData", this.cropper.getData());
-    console.log("getImageData", this.cropper.getImageData());
-    console.log("getCanvasData", this.cropper.getCanvasData());
-    //console.log("getCroppedCanvas", this.cropper.getCroppedCanvas().toDataURL());
-    //let dataurl = this.cropper.getCroppedCanvas().toDataURL("image/png");
-    //window.open(dataurl, "Image");
-    //console.log(dataurl);
-    //window.open(this.cropper.getCroppedCanvas().toDataURL(), '_blank');
-    //this.previewSrc = this.cropper.getCroppedCanvas().toDataURL();
-  }
-
-  public getFile(filename?: string, mimeType: string = 'image/jpeg'): Promise<File> {
-    if (this.cropper) {
-
-      return new Promise((res, rej) => {
-        this.cropper.getCroppedCanvas().toBlob((b => {
-          res(new File([b!], filename || "file.png"));
-        }), mimeType);
-      });
-
-    }
-    
-    return new Promise((res,rej) => rej());
-  }
-
-  public getDataUrl(mimeType: string = 'image/jpeg'): string | undefined {
-    //mimeType = mimeType || 'image/jpeg';
-    if (this.cropper){ 
-        return this.cropper.getCroppedCanvas().toDataURL(mimeType);
+    @bindable public dragMode: Cropper.DragMode = 'move';
+    dragModeChanged(dragMode: Cropper.DragMode) {
+        if (this.cropper?.setDragMode) {
+            this.cropper.setDragMode(dragMode);
+        }
     }
 
-    return undefined;
-  }
+    @bindable({ defaultBindingMode: bindingMode.oneTime }) public responsive: boolean = true;
+    @bindable({ defaultBindingMode: bindingMode.oneTime }) public maxHeight: Number = 85;
 
-  loaded($event) {
-    this.init();
-  }
+    @bindable({ defaultBindingMode: bindingMode.toView }) public options: Cropper.Options<any>;
+    @bindable({ defaultBindingMode: bindingMode.toView }) public src: string;
+    @bindable({ defaultBindingMode: bindingMode.toView }) public title: string;
 
-  private init() {
-    let defaultOptions: Cropper.Options<any> = {
-      viewMode: 0, // 2 //CropperViewMods.CropBoxIsJustWithInTheContainer,
-      dragMode: this.dragMode, //<any>'crop', // crop | move | none
-      //aspectRatio: NaN,
-      data: undefined,
-      responsive: true,
-      restore: true
-    };
+    @bindable({ defaultBindingMode: bindingMode.toView }) public cropStart: (event: CustomEvent) => void;
+    @bindable({ defaultBindingMode: bindingMode.toView }) public cropMove: (event: CustomEvent) => void;
+    @bindable({ defaultBindingMode: bindingMode.toView }) public cropEnd: (event: CustomEvent) => void;
+    @bindable({ defaultBindingMode: bindingMode.toView }) public crop: (event: CustomEvent) => void;
+    @bindable({ defaultBindingMode: bindingMode.toView }) public zoom: (event: CustomEvent) => void;
 
-    if (this.cropStart) defaultOptions.cropstart = (event) => { this.cropStart(event); };
-    if (this.cropMove) defaultOptions.cropmove = (event) => { this.cropMove(event); };
-    if (this.cropEnd) defaultOptions.cropend = (event) => { this.cropEnd(event); };
-    if (this.crop) defaultOptions.crop = (event) => { this.crop(event); };
-    if (this.zoom) defaultOptions.zoom = (event) => { this.zoom(event); };
+    private attached() { }
 
-    let opts = Object.assign(defaultOptions, this.options || {});
+    private detached() {
+        this.cropper.destroy();
+    }
 
-    this.cropper = new Cropper(this.element, opts);
-  }
+    public getData() {
+        console.log("CropBoxData", this.cropper.getCropBoxData());
+        console.log("getData", this.cropper.getData());
+        console.log("getImageData", this.cropper.getImageData());
+        console.log("getCanvasData", this.cropper.getCanvasData());
+    }
+
+    public getFile(filename?: string, mimeType: string = 'image/jpeg'): Promise<File> {
+        if (this.cropper) {
+
+            return new Promise((res, rej) => {
+                this.cropper.getCroppedCanvas().toBlob((b => {
+                    res(new File([b!], filename || "file.png"));
+                }), mimeType);
+            });
+
+        }
+
+        return new Promise((res, rej) => rej());
+    }
+
+    public getDataUrl(mimeType: string = 'image/jpeg'): string | undefined {
+        //mimeType = mimeType || 'image/jpeg';
+        if (this.cropper) {
+            return this.cropper
+                .getCroppedCanvas()
+                .toDataURL(mimeType);
+        }
+
+        return undefined;
+    }
+
+    loaded($event) {
+        this.init();
+    }
+
+    private init() {
+        let defaultOptions: Cropper.Options<any> = {
+            viewMode: 0, // 2 //CropperViewMods.CropBoxIsJustWithInTheContainer,
+            dragMode: this.dragMode, //<any>'crop', // crop | move | none
+            //aspectRatio: NaN,
+            data: undefined,
+            responsive: true,
+            restore: true
+        };
+
+        if (this.cropStart) defaultOptions.cropstart = (event) => { this.cropStart(event); };
+        if (this.cropMove) defaultOptions.cropmove = (event) => { this.cropMove(event); };
+        if (this.cropEnd) defaultOptions.cropend = (event) => { this.cropEnd(event); };
+        if (this.crop) defaultOptions.crop = (event) => { this.crop(event); };
+        if (this.zoom) defaultOptions.zoom = (event) => { this.zoom(event); };
+
+        let opts = Object.assign(defaultOptions, this.options || {});
+
+        this.cropper = new Cropper(this.element, opts);
+    }
 
 }
 
