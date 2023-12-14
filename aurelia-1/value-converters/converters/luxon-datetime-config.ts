8fde63409b2a34bc8a4378197fb95ff16f5cf185
Sondre - EV-H <sondre.erno@evenoit.no>
2023-12-14 11:25:42 +0100

    luxon datetime bugs

---
+++ aurelia-1/value-converters/converters/luxon-datetime-config.ts
@@ -0,0 +1,45 @@
+import { DateTime, DateTimeOptions, Zone } from 'luxon';
+import { IDateTimeValueConverterConfig } from '../interfaces';
+
+const defaultConfig: IDateTimeValueConverterOptions = {
+  defaultFormat: 'dd. LL yyyy',
+  locale: undefined,
+};
+
+export function configure(cb: (config: IDateTimeValueConverterOptions) => void) {
+  cb(defaultConfig);
+}
+
+export function getDate(date: string | Date, opts?: DateTimeOptions) {
+  opts = opts ?? {};
+  if (!opts.locale) { opts.locale = defaultConfig.locale; }
+  if (!opts.zone) { opts.zone = defaultConfig.zone; }
+
+  return typeof date === 'string' ?
+    DateTime.fromISO(date, opts) :
+    DateTime.fromJSDate(date, opts);
+}
+
+export function buildOptions(opts?: IDateTimeValueConverterOptions) {
+  const options = Object.assign({}, defaultConfig, opts ?? {});
+
+  if (!options.format) { options.format = defaultConfig.format ?? defaultConfig.defaultFormat; }
+  if (!options.defaultFormat) { options.format; }
+  
+
+  return options;
+}
+
+export function getDateTimeOptions(opts?: IDateTimeValueConverterOptions) {
+  return {
+    locale: opts?.format ?? opts?.defaultFormat ?? defaultConfig.format ?? defaultConfig.defaultFormat,
+    zone: opts?.zone ?? defaultConfig.zone
+  } as DateTimeOptions;
+}
+
+export interface IDateTimeValueConverterOptions extends IDateTimeValueConverterConfig {
+  format?: string;
+  type?: 'toFormat' | 'toRelative' | 'relativeToCalendar';
+  relativeBase?: string | Date;
+  zone?: string | Zone | undefined;
+}
--- aurelia-1/value-converters/converters/luxon-datetime.ts
+++ aurelia-1/value-converters/converters/luxon-datetime.ts
@@ -1,54 +1,13 @@
 import { valueConverter } from 'aurelia-framework';
-import { DateTime, DateTimeOptions, Zone } from 'luxon';
-import { IDateTimeValueConverterConfig } from '../interfaces';
+import { DateTime } from 'luxon';
+import { IDateTimeValueConverterOptions, buildOptions, getDate, getDateTimeOptions, configure } from './luxon-datetime-config';
 
-const config: IDateTimeValueConverterOptions = {
-  defaultFormat: 'dd. LL yyyy',
-  locale: undefined,
-};
 
-export function setConfig(cb: (config: IDateTimeValueConverterOptions) => void) {
-  cb(this.config);
-}
-
-export function getDate(date: string | Date, opts?: DateTimeOptions) {
-  opts = opts ?? {};
-  if (!opts.locale) { opts.locale = config.locale; }
-  if (!opts.zone) { opts.zone = config.zone; }
-
-  return typeof date === 'string' ?
-    DateTime.fromISO(date, opts) :
-    DateTime.fromJSDate(date, opts);
-}
-
-function buildOptions(opts?: IDateTimeValueConverterOptions) {
-  const options = Object.assign({}, config, opts ?? {});
-
-  if (!options.format) { options.format = config.format ?? config.defaultFormat; }
-  if (!options.defaultFormat) { options.format; }
-  
-
-  return options;
-}
-
-function getDateTimeOptions(opts?: IDateTimeValueConverterOptions) {
-  return {
-    locale: opts?.format ?? opts?.defaultFormat ?? config.format ?? config.defaultFormat,
-    zone: opts?.zone ?? config.zone
-  } as DateTimeOptions;
-}
-
-interface IDateTimeValueConverterOptions extends IDateTimeValueConverterConfig {
-  format?: string;
-  type?: 'toFormat' | 'toRelative' | 'relativeToCalendar';
-  relativeBase?: string | Date;
-  zone?: string | Zone | undefined;
-}
-
-// usage: | DateTime : 'dd.LL.yyyy'
 @valueConverter('datetime')
 export class DatetimeValueConverter {
   toView = DatetimeValueConverter.convert;
+  
+  public static configure = configure;
 
   public static convert(date: string | Date, options?: IDateTimeValueConverterOptions) {
     options = buildOptions(options);
