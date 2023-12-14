9aa3b8b6f50f9e2f35fd8f2d600bc9e8b2c5bfa4
Sondre - EV-H <sondre.erno@evenoit.no>
2023-12-14 10:40:35 +0100

    luxon-datetime valueconverter added options for relative date. Breaking input params

--- aurelia-1/value-converters/converters/luxon-datetime.ts
+++ aurelia-1/value-converters/converters/luxon-datetime.ts
@@ -1,41 +1,79 @@
 import { valueConverter } from 'aurelia-framework';
-import { DateTime, DateTimeOptions } from 'luxon';
+import { DateTime, DateTimeOptions, Zone } from 'luxon';
 import { IDateTimeValueConverterConfig } from '../interfaces';
 
+const config: IDateTimeValueConverterOptions = {
+  defaultFormat: 'dd. LL yyyy',
+  locale: undefined,
+};
+
+export function setConfig(cb: (config: IDateTimeValueConverterOptions) => void) {
+  cb(this.config);
+}
+
+export function getDate(date: string | Date, opts?: DateTimeOptions) {
+  opts = opts ?? {};
+  if (!opts.locale) { opts.locale = config.locale; }
+  if (!opts.zone) { opts.zone = config.zone; }
+
+  return typeof date === 'string' ?
+    DateTime.fromISO(date, opts) :
+    DateTime.fromJSDate(date, opts);
+}
+
+function buildOptions(opts?: IDateTimeValueConverterOptions) {
+  const options = Object.assign({}, config, opts ?? {});
+
+  if (!options.format) { options.format = config.format ?? config.defaultFormat; }
+  if (!options.defaultFormat) { options.format; }
+  
+
+  return options;
+}
+
+function getDateTimeOptions(opts?: IDateTimeValueConverterOptions) {
+  return {
+    locale: opts?.format ?? opts?.defaultFormat ?? config.format ?? config.defaultFormat,
+    zone: opts?.zone ?? config.zone
+  } as DateTimeOptions;
+}
+
+interface IDateTimeValueConverterOptions extends IDateTimeValueConverterConfig {
+  format?: string;
+  type?: 'toFormat' | 'toRelative' | 'relativeToCalendar';
+  relativeBase?: string | Date;
+  zone?: string | Zone | undefined;
+}
 
 // usage: | DateTime : 'dd.LL.yyyy'
 @valueConverter('datetime')
 export class DatetimeValueConverter {
-    toView = DatetimeValueConverter.convert;
+  toView = DatetimeValueConverter.convert;
 
+  public static convert(date: string | Date, options?: IDateTimeValueConverterOptions) {
+    options = buildOptions(options);
+    const dtOptions = getDateTimeOptions(options);
+    const dt = getDate(date, dtOptions);
 
-    public static readonly config: IDateTimeValueConverterConfig = {
-        defaultFormat: 'dd. LL yyyy',
-        locale: undefined
-      }
-      
-      public static setConfig(cb: (config: IDateTimeValueConverterConfig) => void) {
-        cb(this.config);
-      }
-
-      
-
-      public static convert(date: string | Date, format?: string, locale?: string) {
-        if (!format) {
-          format = DatetimeValueConverter.config.defaultFormat;
-        }
-        if (!locale) {
-          locale = DatetimeValueConverter.config.locale;
-        }
-
-        let opts: DateTimeOptions = {};
-        if (locale) { opts.locale = locale; }
-
-        let dt = typeof date === 'string' ?
-            DateTime.fromISO(date, opts) :
-            DateTime.fromJSDate(date, opts);
-
-        return dt.toFormat(format);
+    const type = options?.type?.toLowerCase();
+    if(!type  || type == 'toformat'){
+      return dt.toFormat(options!.format!);
     }
-}
 
+    if(type == 'torelative'){
+      let base: DateTime | undefined = undefined;
+      if(options.relativeBase){
+        base = getDate(options.relativeBase, dtOptions);
+      }
+      return dt.toRelative({ base: base })
+    }
+
+    if(type == 'torelativecalendar'){
+      let base: DateTime | undefined = undefined;
+      if(options.relativeBase){
+        base = getDate(options.relativeBase, dtOptions);
+      }
+      return dt.toRelativeCalendar({ base: base })
+    }
+  }
+}
