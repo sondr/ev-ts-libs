6c6c4490681d1263f63d36fd5c37a01f3a2e4134
Sondre - EV-H <sondre.erno@evenoit.no>
2024-11-26 10:17:42 +0100

    Refactor timeout-intervaller.ts and package.json

--- aurelia-1/common/timeout-intervaller.ts
+++ aurelia-1/common/timeout-intervaller.ts
@@ -2,36 +2,109 @@ import { PLATFORM } from "aurelia-framework";
 
 abstract class TimeIntervalBase {
     protected id?: number;
-    public abstract start<TArgs extends any[]>(handler: TimerHandler, timeout: number, ...args: TArgs);
-    public abstract clear() : TimeIntervalBase;
+    public abstract start(...args: any[]): void;
+
+    public abstract clear(): TimeIntervalBase;
 }
-
-
 
 export class Timeouter extends TimeIntervalBase {
     public start<TArgs extends any[]>(handler: TimerHandler, ms: number, ...args: TArgs) {
-        this.id = (PLATFORM.global as Window & typeof globalThis).setTimeout(handler, ms, args);
+        this.id = (PLATFORM.global as Window & typeof globalThis).setTimeout(handler, ms, ...args);
     }
 
     public clear() {
-        (PLATFORM.global as Window & typeof globalThis).clearTimeout(this.id);
-        this.id = undefined;
+        if (this.id != undefined) {
+            (PLATFORM.global as Window & typeof globalThis).clearTimeout(this.id);
+            this.id = undefined;
+        }
 
         return this;
     }
 }
 
-
-
-
 export class Intervaller extends TimeIntervalBase {
-    public start<TArgs extends any[]>(handler: TimerHandler, timeout: number, ...args) {
-        (PLATFORM.global as Window & typeof globalThis).setInterval(handler, timeout, args);
+    public start<TArgs extends any[]>(handler: TimerHandler, timeout: number, ...args: TArgs) {
+        this.id = (PLATFORM.global as Window & typeof globalThis).setInterval(handler, timeout, ...args);
     }
 
     public clear() {
-        (PLATFORM.global as Window & typeof globalThis).clearInterval(this.id);
+        if (this.id != undefined) {
+            (PLATFORM.global as Window & typeof globalThis).clearInterval(this.id);
+            this.id = undefined;
+        }
 
+        return this;
+    }
+}
+
+export class AnimationFrameScheduler extends TimeIntervalBase {
+    public start(handler: FrameRequestCallback): void {
+        this.id = (PLATFORM.global as Window & typeof globalThis).requestAnimationFrame(handler);
+    }
+
+    public clear(): TimeIntervalBase {
+        if (this.id !== undefined) {
+            (PLATFORM.global as Window & typeof globalThis).cancelAnimationFrame(this.id);
+            this.id = undefined;
+        }
+
+        return this;
+    }
+}
+
+export class WorkerTimer extends TimeIntervalBase {
+    private worker?: Worker;
+
+    public start(handler: TimerHandler, timeout: number): void {
+        if (typeof handler !== 'function') {
+            throw new Error('Handler must be a function for WorkerTimer.');
+        }
+
+        const blob = new Blob([`
+            self.onmessage = function(e) {
+                setTimeout(() => postMessage(null), e.data);
+            }
+        `], { type: 'application/javascript' });
+
+        this.worker = new Worker(URL.createObjectURL(blob));
+        this.worker.onmessage = () => handler();
+        this.worker.postMessage(timeout);
+    }
+
+    public clear(): TimeIntervalBase {
+        if (this.worker) {
+            this.worker.terminate();
+            this.worker = undefined;
+        }
+
+        return this;
+    }
+}
+
+export class PromiseTimer extends TimeIntervalBase {
+    private cancel?: () => void;
+
+    public start<TArgs extends any[]>(handler: TimerHandler, timeout: number, ...args: TArgs): void {
+        if (typeof handler !== 'function') {
+            throw new Error('Handler must be a function for WorkerTimer.');
+        }
+
+        let canceled = false;
+
+        this.cancel = () => { canceled = true; };
+
+        new Promise<void>((resolve) => {
+            setTimeout(() => {
+                if (!canceled) resolve();
+            }, timeout);
+        }).then(() => handler(...args));
+    }
+
+    public clear(): TimeIntervalBase {
+        if (this.cancel) {
+            this.cancel();
+            this.cancel = undefined;
+        }
         return this;
     }
 }
